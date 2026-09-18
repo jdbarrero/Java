53df243654c098bb4e753dbdfff91e5ced96d00a
jdbarrero <jdbarrero@uniquindio.edu.co>
2026-09-17 19:23:32 -0500

    Delete logica-programacion-java/04-funciones directory

--- logica-programacion-java/04-funciones/Ejemplo1FuncionSimple.java
+++
@@ -1,9 +0,0 @@
-public class Ejemplo1FuncionSimple {
-    static void mostrarBienvenida() {
-        System.out.println("Bienvenido al curso de Java.");
-    }
-
-    public static void main(String[] args) {
-        mostrarBienvenida();
-    }
-}
